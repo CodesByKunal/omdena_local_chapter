73296465fbd1f6d7ed5df4920d22960a73249cac
Kunal Kumar <8055kunalkumar@gmail.com>
2023-10-08 11:24:25 +0530

    Auto Hide Side Nav

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ const sideNav = document.getElementById('side-nav');
 
 menuIcon.addEventListener('click', () => {
     sideNav.classList.toggle('nav-slide-in');
-    
+
     if (sideNav.classList.contains('nav-slide-out')) {
         sideNav.classList.remove('nav-slide-out');
     } else {
         sideNav.classList.add('nav-slide-out');
     }
 });
+document.addEventListener('click', (e) => {
+    console.log(e.target)
+    if (!e.target.classList.contains('tagName')) {
+        sideNav.classList.remove('nav-slide-in');
+        sideNav.classList.add('nav-slide-out');
+    }
+})
